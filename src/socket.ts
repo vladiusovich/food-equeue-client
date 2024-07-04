48cbd509b8338cd53ea86556711e4e00eb517aba
uladzislau usovich <vladiusovich@gmail.com>
2024-07-04 22:03:14 +0400

    feat: added socketUrl to config

--- src/socket.ts
+++ src/socket.ts
@@ -1,9 +1,7 @@
 import { io } from 'socket.io-client';
+import appConfig from './config/appConfig';
 
-// TODO: Change the port when deploying to production
-const PORT = 3002;
-
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : `http://localhost:${PORT}`;
+// TODO: Change it when deploying to production
+const URL = process.env.NODE_ENV === 'production' ? undefined : appConfig.socketUrl;
 
 export const socket = io(URL!);
